7335ee57e427bc1762992ab986ca9c05ada03ec4
Jason Jolting <30330652+zjojo@users.noreply.github.com>
2019-01-18 16:39:23 +0800

    Update config.py

--- A09_create_data/select_data_to_redis/config.py
+++ A09_create_data/select_data_to_redis/config.py
@@ -1,7 +1,7 @@
 # db_config = {
 #         "host":"127.0.0.1",
 #         "user":"root",
-#         "passwd":"ptdAChu+byhzq2dCc0&MLds",
+#         "passwd":"root",
 #         "db":"camel_test",
 #         "port":3306,
 #         "charset":"utf8"
@@ -9,7 +9,7 @@
 db_config = {
         "host":"127.0.0.1",
         "user":"root",
-        "passwd":"jolting823",
+        "passwd":"root",
         "db":"test",
         "port":3306,
         "charset":"utf8"
