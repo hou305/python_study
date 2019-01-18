69997c4d6eb19538a69652fd26841c52067e0bfd
Jason Jolting <30330652+zjojo@users.noreply.github.com>
2019-01-18 16:38:11 +0800

    Update config.py

--- A09_create_data/insert_data_to_database/config.py
+++ A09_create_data/insert_data_to_database/config.py
@@ -10,7 +10,7 @@ import os
 db_config = {
         "host":"127.0.0.1",
         "user":"root",
-        "passwd":"jolting823",
+        "passwd":"root",
         "db":"test",
         "port":3306,
         "charset":"utf8"
